57f99b1d31fc2ec09e965cfe59bfcb35162bc405
Kimgmanis <Kimalexanderkeiser@gmail.com>
2022-02-24 14:09:12 +0100

    Completed Chinook

--- DB_Exercise_Set_1/Chinook MySQL/ChinookMySQLdml.sql
+++ DB_Exercise_Set_1/Chinook MySQL/ChinookMySQLdml.sql
@@ -1,8 +1,83 @@
 # 1. How many songs are there in the playlist “Grunge”?
-SELECT COUNT(*) FROM chinook.playlist
+SELECT COUNT(*)
+FROM chinook.playlist
 WHERE playlist.Name = 'Grunge';
 
-SELECT COUNT(*) FROM chinook.playlisttrack
+SELECT COUNT(*)
+FROM chinook.playlisttrack
 WHERE PlaylistId = 16;
 
 # 2. Show information about artists whose name includes the text “Jack” and about artists whose name includes the text “John”, but not the text “Martin”.
+SELECT *
+FROM chinook.artist
+WHERE artist.Name LIKE 'Jack%'
+   OR artist.Name LIKE 'John%'
+   OR artist.Name LIKE '!Martin';
+
+# 3. For each country where some invoice has been issued, show the total invoice monetary amount, but only for countries where at least $100 have been invoiced. Sort the information from higher to lower monetary amount.
+SELECT BillingCountry, SUM(Total) AS "Total invoice monetary amount"
+FROM invoice
+GROUP BY BillingCountry
+HAVING `Total invoice monetary amount` >= 100
+ORDER BY `Total invoice monetary amount` DESC;
+
+# 4. Get the phone number of the boss of those employees who have given support to clients who have bought some song composed by “Miles Davis” in “MPEG Audio File” format.
+SELECT Phone
+FROM employee
+WHERE EmployeeId in
+      (SELECT ReportsTo
+       FROM employee
+       WHERE (EmployeeId in
+              (SELECT SupportRepId
+               FROM customer
+               WHERE CustomerId in
+                     (SELECT CustomerId
+                      FROM invoice
+                      WHERE InvoiceId in
+                            (SELECT InvoiceId
+                             FROM invoiceline
+                             WHERE TrackId in
+                                   (SELECT TrackId
+                                    FROM track
+                                    WHERE Composer = 'Miles Davis'
+                                      AND MediaTypeId =
+                                          (SELECT MediaTypeId FROM mediatype WHERE Name = 'MPEG Audio File')))))));
+
+#5 Show the information, without repeated records, of all albums that feature songs of the “Bossa Nova” genre whose title starts by the word “Samba”.
+SELECT DISTINCT Album.*
+FROM Album
+         JOIN Track ON Album.AlbumId = Track.AlbumId
+WHERE GenreId = 11;
+
+# 6. For each genre, show the average length of its songs in minutes (without indicating seconds). Use the headers “Genre” and “Minutes”, and include only genres that have any song longer than half an hour.
+SELECT DISTINCT Genre.Name AS 'Genre', ROUND(AVG(Track.Milliseconds / 60000), 0) AS 'Minutes'
+FROM Track,
+     Genre
+WHERE Genre.GenreId = Track.GenreId
+GROUP BY Genre.GenreId
+HAVING Minutes > 30;
+
+# 7. How many client companies have no state?
+SELECT *
+FROM Customer
+WHERE Company IS NOT NULL
+  AND State IS NULL;
+
+# 8. For each employee with clients in the “USA”, “Canada” and “Mexico” show the number of clients from these countries s/he has given support, only when this number is higher than 6. Sort the query by number of clients. Regarding the employee, show his/her first name and surname separated by a space. Use “Employee” and “Clients” as headers.
+SELECT CONCAT(Employee.FirstName, " ", Employee.LastName) AS Employee, count(*) AS Clients
+FROM Employee
+         LEFT JOIN Customer C on Employee.EmployeeId = C.SupportRepId
+WHERE C.Country IN ('USA', 'Mexico', 'Canada')
+GROUP BY Employee.FirstName, Employee.LastName
+HAVING count(*) > 6
+ORDER BY count(*) DESC;
+
+# 9. For each client from the “USA”, show his/her surname and name (concatenated and separated by a comma) and their fax number. If they do not have a fax number, show the text “S/he has no fax”. Sort by surname and first name.
+SELECT CONCAT(LastName, ", ", FirstName) AS Name, IFNULL(Fax, 'S/he has no fax') AS Fax
+FROM Customer
+WHERE Country = 'USA'
+ORDER BY LastName, FirstName;
+
+# 10. For each employee, show his/her first name, last name, and their age at the time they were hired.
+SELECT FirstName, LastName, DATEDIFF(HireDate, BirthDate) / 365 AS 'Age @ time of hire in years'
+FROM Employee;
--- DB_Exercise_Set_1/Library MySQL/LibraryMySQLdml.sql
+++ DB_Exercise_Set_1/Library MySQL/LibraryMySQLdml.sql
@@ -1,40 +1,82 @@
 # 1. Show the members under the name "Jens S." who were born before 1970 that became members of the library in 2013.
-SELECT * FROM library.tmember
-WHERE cName REGEXP 'Jens S.' AND dBirth < '1970-01-01' AND dNewMember > '2013-01-01';
+SELECT *
+FROM library.tmember
+WHERE cName REGEXP 'Jens S.'
+  AND dBirth < '1970-01-01'
+  AND dNewMember > '2013-01-01';
 
 # 2. Show those books that have not been published by the publishing companies with ID 15 and 32, except if they were published before 2000.
-SELECT * FROM library.tbook
-WHERE nPublishingCompanyID IN (15,32) AND nPublishingYear < 2000;
+SELECT *
+FROM library.tbook
+WHERE nPublishingCompanyID IN (15, 32)
+  AND nPublishingYear < 2000;
 
 # 3. Show the name and surname of the members who have a phone number, but no address.
-SELECT cName, cSurname FROM library.tmember
-WHERE cAddress IS NULL AND cPhoneNo IS NOT NULL;
+SELECT cName, cSurname
+FROM library.tmember
+WHERE cAddress IS NULL
+  AND cPhoneNo IS NOT NULL;
 
 # 4. Show the authors with surname "Byatt" whose name starts by an "A" (uppercase) and contains an "S" (uppercase).
-SELECT * FROM library.tauthor
-WHERE cSurname REGEXP 'Byatt' AND cName REGEXP '^A|S]';
+SELECT *
+FROM library.tauthor
+WHERE cSurname REGEXP 'Byatt'
+  AND cName REGEXP '^A|S]';
 
 # 5. Show the number of books published in 2007 by the publishing company with ID 32.
-SELECT * FROM library.tbook
-WHERE nPublishingYear = 2007 AND nPublishingCompanyID = 32;
+SELECT *
+FROM library.tbook
+WHERE nPublishingYear = 2007
+  AND nPublishingCompanyID = 32;
 # OR
-SELECT COUNT(*) FROM library.tbook
-WHERE nPublishingYear = 2007 AND nPublishingCompanyID = 32;
+SELECT COUNT(*)
+FROM library.tbook
+WHERE nPublishingYear = 2007
+  AND nPublishingCompanyID = 32;
 
 # 6. For each day of the year 2014, show the number of books loaned by the member with CPR "0305393207";
-SELECT * FROM library.tloan
-WHERE cCPR = 0305393207 AND dLoan > '2014-01-01' AND dLoan < '2015-01-01';
+SELECT *
+FROM library.tloan
+WHERE cCPR = 0305393207
+  AND dLoan > '2014-01-01'
+  AND dLoan < '2015-01-01';
 # OR
-SELECT COUNT(*) FROM library.tloan
-WHERE cCPR = 0305393207 AND dLoan > '2014-01-01' AND dLoan < '2015-01-01';
+SELECT COUNT(*)
+FROM library.tloan
+WHERE cCPR = 0305393207
+  AND dLoan > '2014-01-01'
+  AND dLoan < '2015-01-01';
 
 # 7. Modify the previous clause so that only those days where the member was loaned more than one book appear.
-SELECT * FROM library.tloan
-WHERE cCPR = 0305393207 AND dLoan > '2014-01-01' AND dLoan < '2015-01-01';
+SELECT dloan, COUNT(dloan) AS 'Number of Loans'
+FROM tloan
+WHERE cCPR = 0305393207
+  AND YEAR(dLoan) = 2014
+GROUP BY dloan
+HAVING COUNT(*) > 1;
 
-SELECT cSignature FROM tloan
-WHERE cCPR ='0305393207' AND dLoan LIKE '2014-%' GROUP BY dLoan HAVING COUNT(*) > 1;
+# 8. Show all library members from the newest to the oldest. Those who became members on the same day will be sorted alphabetically (by surname and name) within that day.
+SELECT *
+FROM library.tmember
+ORDER BY dNewMember DESC, cSurname, cName;
 
-SELECT * FROM library.tloan
-WHERE cCPR = 0305393207 AND dLoan LIKE '2014-%';
+# 9. Show the title of all books published by the publishing company with ID 32 along with their theme or themes.
+SELECT tbook.cTitle, ttheme.cName
+FROM tbook,
+     tbooktheme,
+     ttheme
+WHERE nPublishingCompanyID = 32;
+
+# 10. Show the name and surname of every author along with the number of books authored by them, but only for authors who have registered books on the database.
+SELECT cName, cSurname, COUNT(t.nAuthorID) AS 'Books'
+FROM tauthor
+         LEFT JOIN tauthorship t on tauthor.nAuthorID = t.nAuthorID
+GROUP BY tauthor.nAuthorID
+ORDER BY COUNT(t.nAuthorID) DESC;
+
+# 11. Show the name and surname of all the authors with published books along with the lowest publishing year for their books.
+
+
+
+
 
