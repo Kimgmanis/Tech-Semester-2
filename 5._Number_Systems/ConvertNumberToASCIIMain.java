29fe4eec493e2053008989ed64d869cb183dccd9
Kimgmanis <Kimalexanderkeiser@gmail.com>
2022-04-22 12:36:56 +0200

    Created Kea Shop

---
+++ 5._Number_Systems/ConvertNumberToASCIIMain.java
@@ -0,0 +1,10 @@
+public class ConvertNumberToASCIIMain {
+    public static void main(String[] args) {
+        // char asciiSymbol = (char) 65;
+        // TODO save to a database
+        for (int i = 32; i < 123; i++) {
+            System.out.print((char) i);
+        }
+    }
+}
+
